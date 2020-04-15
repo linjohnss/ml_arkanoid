cfc1f7d5ab7989c091d2390fc4b737f348baea10
linjohnss <a0920594352@gmail.com>
2020-04-16 02:14:54 +0800

    movee

--- ml_play.py
+++ ml_play.py
@@ -4,7 +4,6 @@ The template of the main script of the machine learning process
 """
 import pickle
 import numpy as np
-import random
 import games.arkanoid.communication as comm
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
@@ -64,14 +63,6 @@ def ml_loop():
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
-        if (scene_info.frame < 150):
-            n = random.randint(0, 2)
-            if n == 0:
-                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
-            if n == 1:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            elif n == 2:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
         feature = [] #數量內容要一致
         feature.append(scene_info.ball[0])
         feature.append(scene_info.ball[1])
