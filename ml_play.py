205219c67999987b6d09e5f2c28133802a153454
linjohnss <a0920594352@gmail.com>
2020-04-15 16:19:45 +0800

    ml_aa

--- ml_play.py
+++ ml_play.py
@@ -1,12 +1,15 @@
+
 """
 The template of the main script of the machine learning process
 """
-
+import pickle
+import numpy as np
 import games.arkanoid.communication as comm
-import random
 from games.arkanoid.communication import ( \
     SceneInfo, GameStatus, PlatformAction
 )
+import os.path as path
+
 
 def ml_loop():
     """
@@ -19,42 +22,57 @@ def ml_loop():
     is behind of the current frame in the game process. Try to decrease the fps
     to avoid this situation.
     """
-
+    #放ml裡面
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
-
-    ballpos=[]
-    #記錄球的位置去解算斜率
+    filename = path.join(path.dirname(__file__),"save\clf_KMeans_BallAndDirection.pickle")
+    with open(filename, 'rb') as file:
+        clf = pickle.load(file)
 
     # 2. Inform the game process that ml process is ready before start the loop.
+
+    s = [93, 93]
+
+    def get_direction(ball_x, ball_y, ball_pre_x, ball_pre_y):
+        VectorX = ball_x - ball_pre_x
+        VectorY = ball_y - ball_pre_y
+        if (VectorX >= 0 and VectorY >= 0):
+            return 0
+        elif (VectorX > 0 and VectorY < 0):
+            return 1
+        elif (VectorX < 0 and VectorY > 0):
+            return 2
+        elif (VectorX < 0 and VectorY < 0):
+            return 3
+
+    def get_direction_x(ball_x, ball_pre_x):
+        VectorX = ball_x - ball_pre_x
+        return VectorX
+
+    def get_direction_y(ball_y, ball_pre_y):
+        VectorY = ball_y - ball_pre_y
+        return VectorY
+
     comm.ml_ready()
+    
+
+        
 
     # 3. Start an endless loop.
     while True:
         # 3.1. Receive the scene information sent from the game process.
-        scene_info = comm.get_scene_info()  #讀取遊戲資料
-        ballpos.append(scene_info.ball)     #
-        platform_center_x = scene_info.platform[0]+20
-
-        if(scene_info.frame<150):
-            n = random.randint(0, 2)
-            if n == 0:
-                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
-            if n == 1:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-            elif n == 2:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-
-        if(len(ballpos))==1:
-            ball_going_down = 0
-        elif ballpos[-1][1]-ballpos[-2][1] > 0:
-            ball_going_down = 1
-            vy=ballpos[-1][1]-ballpos[-2][1]
-            vx=ballpos[-1][0]-ballpos[-2][0]
-        else:
-            ball_going_down = 0
-
+        scene_info = comm.get_scene_info()
+        feature = [] #數量內容要一致
+        feature.append(scene_info.ball[0])
+        feature.append(scene_info.ball[1])
+        feature.append(scene_info.platform[0])
+        feature.append(get_direction_x(feature[0],s[0]))
+        feature.append(get_direction_y(feature[1],s[1]))
+        feature.append(get_direction(feature[0],feature[1],s[0],s[1]))
+        s = [feature[0], feature[1]]
+        feature = np.array(feature)
+        feature = feature.reshape((-1,6))
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
@@ -67,35 +85,21 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-        if ball_going_down == 1 :
-            ball_destination = int(ballpos[-1][0] + ((395 - ballpos[-1][1])/vy)*vx)
-            if ball_destination >= 195:
-                ball_destination = 195 - (ball_destination - 195)
-            elif ball_destination <= 0:
-                ball_destination = - ball_destination
-        else:
-            ball_destination = platform_center_x
 
         # 3.4. Send the instruction for this frame to the game process
-
-        if ball_going_down == 1:
-            if platform_center_x - ball_destination <20 and platform_center_x - ball_destination >-20:
+        if not ball_served:
+            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
+            ball_served = True
+        else:
+                #訓練目標
+            y = clf.predict(feature)
+            
+            if y == 0:
                 comm.send_instruction(scene_info.frame, PlatformAction.NONE)
-            else:
-                if platform_center_x - ball_destination>0:
-                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-                elif platform_center_x - ball_destination<0:
-                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
-
-        #if not ball_served:
-        #    comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
-        #    ball_served = True
-        #else:
-        #    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
-
-        #if load_model.predict(input)==1:
-            #comm.send_instruction(scene_info.frame, GameInstruction.CMD_RIGHT)
-        #elif load_model.predict(input)==-1:
-            #comm.send_instruction(scene_info.frame, GameInstruction.CMD_LEFT)
-        #else:
-            #comm.send_instruction(scene_info.frame, GameInstruction.CMD_NONE)
+                print('NONE')
+            elif y == 1:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                print('LEFT')
+            elif y == 2:
+                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                print('RIGHT')
